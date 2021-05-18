fa7387ab9d60fe71b0fe1b4913e4ba3ce805c067
islomnumanov <islom4ik.numanov@gmail.com>
2021-05-18 14:34:46 +0900

    getting ISS API and getting access to the current longitude and latitude

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 import requests
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# print(response)
+# print(response.status_code)
+
+# # in case we omit a letter from the url then we get the following response:
+# response = requests.get(url="http://api.open-notify.org/is-now.json")
+# print(response)
+# print(response.status_code)
+
+# in order to deal with the errors, we can specify their type and details as follows:
+
+# response = requests.get(url="http://api.open-notify.org/is-now.json")
+# response.raise_for_status()
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+
+data = response.json()
+
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_position = (longitude, latitude)
+print(iss_position)
